ba99ccd3106a8e9bf0a53959f9643094e586623c
mayuresh <mayuresh@gmail.com>
2015-03-02 12:29:07 +0530

    Added port

--- api/hello.py
+++ api/hello.py
@@ -1,5 +1,6 @@
 __author__ = 'mayureshp'
 
+import os
 from flask import Flask
 app = Flask(__name__)
 
@@ -8,4 +9,4 @@ def hello():
     return "Hello World!"
 
 if __name__ == "__main__":
-    app.run()
+    app.run(port= os.environ["PORT"] | 5000)
